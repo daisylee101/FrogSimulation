a2a91b11ee2de3cccbd2c998fabc9edbf0d5f613
daisylee101 <daisyl43@nycstudents.net>
2025-05-05 04:41:51 +0000

    donezoooo

--- Main.java
+++ Main.java
@@ -1,1 +1,7 @@
-hellloooooo
+public class Main{
+    public static void main(String[] args) {
+        FrogSimulation sim = new FrogSimulation(24, 5);
+        System.out.println(sim.simulate());
+        System.out.println(sim.runSimulations(4));
+    }
+}
